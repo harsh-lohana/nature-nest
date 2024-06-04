d41691ce6da1e114605e5ba947043dbbc8988180
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2024-06-04 23:08:49 +0530

    Initial

---
+++ components/index.js
@@ -0,0 +1,6 @@
+export { default as Adventures } from "./Adventures";
+export { default as Hero } from "./Hero";
+export { default as Navbar } from "./Navbar";
+export { default as PopularDestination } from "./PopularDestination";
+export { default as Suggestions } from "./Suggestions";
+export { default as Testimonials } from "./Testimonials";

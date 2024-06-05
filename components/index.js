838f2b05fc1f071718e7afb62aa620aa32ed0a76
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2024-06-05 13:09:47 +0530

    BUG: Fixed can't resolve Navbar

--- components/index.js
+++ components/index.js
@@ -1,4 +1,3 @@
 export { default as Hero } from "./Hero";
-export { default as Navbar } from "./Navbar";
 export { default as Testimonials } from "./Testimonials";
 export { default as CardSection } from "./CardSection";

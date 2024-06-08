98c78cafbba78d52a9b24256fec122b516d04efd
Pratyush Dehury <98162702+pratyushd0308@users.noreply.github.com>
2024-06-08 12:49:02 +0530

    Added a footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,19 @@ import {
   NavbarCollapse,
   NavbarLink,
   NavbarToggle,
+  Footer,
+  FooterCopyright,
+  FooterIcon,
+  FooterLink,
+  FooterLinkGroup,
+  FooterTitle,
+  FooterBrand,
 } from "flowbite-react";
 import Link from "next/link";
 import Head from "next/head";
 import { IoLeafSharp } from "react-icons/io5";
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa";
+import { FaXTwitter, FaGithub } from "react-icons/fa6";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -73,6 +82,51 @@ export default function RootLayout({
             </NavbarCollapse>
           </Navbar>
           {children}
+          <Footer bgDark className="rounded-none">
+            <div className="w-full">
+              <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+                <div className="bg-transparent justify-center items-center gap-4 hidden md:flex">
+                  <IoLeafSharp className="text-white size-10" />
+                  <p className="text-2xl md:text-3xl text-white text-center">
+                    Nature India
+                  </p>
+                </div>
+                <div>
+                  <FooterTitle title="Company" />
+                  <FooterLinkGroup col>
+                    <FooterLink href="#">About</FooterLink>
+                    <FooterLink href="#">Contact Us</FooterLink>
+                  </FooterLinkGroup>
+                </div>
+                <div>
+                  <FooterTitle title="help center" />
+                  <FooterLinkGroup col>
+                    <FooterLink href="#">Discord Server</FooterLink>
+                    <FooterLink href="#">Twitter</FooterLink>
+                    <FooterLink href="#">Facebook</FooterLink>
+                    <FooterLink href="#">Contact Us</FooterLink>
+                  </FooterLinkGroup>
+                </div>
+                <div>
+                  <FooterTitle title="legal" />
+                  <FooterLinkGroup col>
+                    <FooterLink href="#">Privacy Policy</FooterLink>
+                    <FooterLink href="#">Licensing</FooterLink>
+                    <FooterLink href="#">Terms &amp; Conditions</FooterLink>
+                  </FooterLinkGroup>
+                </div>
+              </div>
+              <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
+                <FooterCopyright href="#" by="Nature India™" year={2024} />
+                <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+                  <FooterIcon href="#" icon={FaFacebookSquare} />
+                  <FooterIcon href="#" icon={FaInstagram} />
+                  <FooterIcon href="#" icon={FaXTwitter} />
+                  <FooterIcon href="#" icon={FaGithub} />
+                </div>
+              </div>
+            </div>
+          </Footer>
         </div>
       </body>
     </html>
